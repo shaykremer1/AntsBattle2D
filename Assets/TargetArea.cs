7d347a6c8d11d33746dc8c9210735dcf22c61455
Alon Zayger <alonion54@gmail.com>
2024-12-18 01:52:45 +0200

    test2

--- Assets/TargetArea.cs
+++ Assets/TargetArea.cs
@@ -7,6 +7,7 @@ public class TargetArea : MonoBehaviour
 {
     private static List<int> playersWhoDelivered = new List<int>(); // רשימה של השחקנים שהביאו את האוכל
 
+    //test
     // כאשר האוכל נכנס לאזור המטרה
     private void OnTriggerEnter2D(Collider2D collision)
     {
